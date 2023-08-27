24cab4a28ef47c537fbf594fabab4f70fe471bbb
ibnu choldun <ibnucholdun023@gmail.com>
2023-08-27 21:44:55 +0700

    Rendering Lists

--- src/Components/Fragments/CardProduct.jsx
+++ src/Components/Fragments/CardProduct.jsx
@@ -4,7 +4,7 @@ import Button from "../Elements/Button/Index";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow m-2">
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow m-2 flex flex-col justify-between">
       {children}
     </div>
   );
@@ -22,7 +22,7 @@ const Header = (props) => {
 const Body = (props) => {
   const { title, children } = props;
   return (
-    <div className="px-5 pb-5">
+    <div className="px-5 pb-5 h-full">
       <Link to="#">
         <h5 className="text-xl font-semibold tracking-tight text-white">
           {title}
--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,18 +1,38 @@
 import CardProduct from "../Components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    title: "Sepatu Baru",
+    price: "Rp 200.000",
+    image: "/images/product-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        eos numquam rerum recusandae dicta. Reprehenderit ullam iure earum
+        facere ratione sed labore, laudantium expedita placeat quo nobis.
+        Recusandae, fugit dolor.`,
+  },
+  {
+    id: 2,
+    title: "Sepatu Lama",
+    price: "Rp 100.000",
+    image: "/images/product-1.jpg",
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        eos numquam rerum recusandae dicta.`,
+  },
+];
+
 const ProductPage = () => {
   return (
     <div className="flex justify-center py-5 flex-wrap">
-      <CardProduct>
-        <CardProduct.Header image="/images/product-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          eos numquam rerum recusandae dicta. Reprehenderit ullam iure earum
-          facere ratione sed labore, laudantium expedita placeat quo nobis.
-          Recusandae, fugit dolor.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp 200.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body title={product.title}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
