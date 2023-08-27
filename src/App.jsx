53942cd3e509e327b9948703ee2ab98cb3ad8867
ibnu choldun <ibnucholdun023@gmail.com>
2023-08-27 18:14:09 +0700

    Component & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+const Button = (props) => {
+  const { children, variant } = props;
+  return (
+    <button
+      className={`h-10 px-4 rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
 const App = () => {
   return (
-    <div className="flex justify-center min-h-screen items-center">App</div>
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-red-500">Primary</Button>
+        <Button variant="bg-blue-500">Secondary</Button>
+      </div>
+    </div>
   );
 };
 
